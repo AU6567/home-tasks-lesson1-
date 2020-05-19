2ab6a02ccef6b0a80fc421218681faf60e525a59
Anton_U <utkin6567@gmail.com>
2020-05-19 14:48:07 +0300

    Lesson3-HT

--- Task3.py
+++ Task3.py
@@ -1,26 +1,9 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3.
-# Считаем 3 + 33 + 333 = 369.
+# 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+# и возвращает сумму наибольших двух аргументов.
+def my_func():
+    arg1 = float(input("Введите первый аргумент(число): "))
+    arg2 = float(input("Введите второй аргумент(число): "))
+    arg3 = float(input("Введите третий аргумент(число): "))
+    return arg1 + arg2 + arg3 - min(arg1, arg2, arg3)
 
-# n = int(input('Введите число n: '))
-#
-# num1 = int("%s" % n)
-# num2 = int("%s%s" % (n, n))
-# num3 = int("%s%s%s" % (n, n, n))
-# print(num1 + num2 + num3)
-
-# n = int(input('Введите число n: '))
-# sum_n = str(n)
-#
-# num1 = sum_n + sum_n
-# num2 = sum_n + sum_n + sum_n
-#
-# total = n + int(num1) + int(num2)
-# print('Ваше число: ', total)
-
-n = input('Введите число n: ')
-nn = int(n + n)
-nnn = int(n + n + n)
-n = int(n)
-result = n + nn + nnn
-print(result)
+print("Сумма двух наибольших аргументов = ", my_func())
--- Task5.py
+++ Task5.py
@@ -1,20 +1,58 @@
-# 5. Запросите у пользователя значения выручки и издержек фирмы.
-# Определите, с каким финансовым результатом работает фирма
-# (прибыль — выручка больше издержек, или убыток — издержки больше выручки).
-# Выведите соответствующее сообщение. Если фирма отработала с прибылью,
-# вычислите рентабельность выручки (соотношение прибыли к выручке).
-# Далее запросите численность сотрудников фирмы и определите прибыль фирмы
-# в расчете на одного сотрудника.
-revenue = int(input('Введите сумму Вашей прибыли: '))
-cost = int(input('Введите сумму Ваших издержек: '))
-employee = int(input('Введите количество сотрудников фирмы: '))
+# 5. Программа запрашивает у пользователя строку чисел, разделенных пробелом.
+# При нажатии Enter должна выводиться сумма чисел. Пользователь может продолжить ввод чисел,
+# разделенных пробелом и снова нажать Enter.
+# Сумма вновь введенных чисел будет добавляться к уже подсчитанной сумме.
+# Но если вместо числа вводится специальный символ, выполнение программы завершается.
+# Если специальный символ введен после нескольких чисел,
+# то вначале нужно добавить сумму этих чисел к полученной ранее сумме и
+# после этого завершить программу.
+# def sum(line_number):
+#     line_number = line_number.split()
+#     line_number2 = []
+#     for i in line_number:
+#         line_number2.append((int(i)))
+#     return sum(line_number2)
+#
+# x = 0
+# while 1:
+#     line_number = input("Введите строку чисел, разделенных пробелом: ")
+#     if line_number.endswith('*'):
+#         line_number = line_number[:-1]
+#         x += sum(line_number)
+#         print("Сумма чисел: ", x)
+#         break
+#     x += sum(line_number)
+#     print("Сумма чисел: ", x)
 
-if revenue > cost:
-    print('Выручка больше издержек')
-    net_profit = revenue - cost
-    profit = net_profit / revenue
-    print('Рентабельность выручки компании {}: {:.2f}' .format('составила', profit))
-    profit_per_employee = float(net_profit / employee)
-    print('Прибыль фирмы в расчете на одного сотрудника: %s' % profit_per_employee)
-else:
-    print('Фирма работает в убыток - издержки больше выручки')
+def sum_of_number_in_list(list):
+    b = 0
+    for i in list:
+        if i == '*':
+            break
+        else:
+            try:
+                a = int(i)
+                b += a
+            except ValueError:
+                print('Не вводите больше одного пробела между буквами, или не ставьте пробел в конце!\n'
+                      'Или при выходе перед * не ставьте пробел')
+
+    return b
+
+growing_number = 0
+
+while True:
+    ask_numbers_row = input('Введите строку чисел, разделенных пробелом! Или нажмите * для выхода: ')
+
+    if '*' in ask_numbers_row:
+        my_list = ask_numbers_row.split(' ')
+        result = sum_of_number_in_list(my_list)
+        growing_number += result
+        print(growing_number)
+        break
+
+    else:
+        my_list = ask_numbers_row.split(' ')
+        result = sum_of_number_in_list(my_list)
+        growing_number += result
+        print(growing_number)
